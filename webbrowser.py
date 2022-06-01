ac35dedc00be96cdd9eacc296d6e4e36968bf4a0
Jens Shumway <jensshum@gmail.com>
2022-06-01 17:47:00 -0600

    Reached download stage

--- webbrowser.py
+++ webbrowser.py
@@ -1,3 +1,4 @@
+from cgitb import html
 from gettext import find
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,6 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 # import org.openqa.selenium.WebElement;
 # import org.openqa.selenium.chrome.ChromeDriver;
 from time import sleep
+from datetime import timedelta
+import datetime
+
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -26,12 +30,12 @@ search2.send_keys("Gofastgas22")
 search2.send_keys(Keys.RETURN)
 sleep(3)
 try:
-    html_path = '''//div[@onclick="javascript:window.parent.showLoader();reDirectTo('/OfficeAdmin/reports2.aspx')"]'''
+    Xpath = '''//div[@onclick="javascript:window.parent.showLoader();reDirectTo('/OfficeAdmin/reports2.aspx')"]'''
     search = driver.switch_to.frame("RAD_SPLITTER_PANE_EXT_CONTENT_ContentPane")
     element = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.XPATH, html_path))
+        EC.presence_of_element_located((By.XPATH, Xpath))
     )
-    search = driver.find_element(By.XPATH, html_path)
+    search = driver.find_element(By.XPATH, Xpath)
     print('path found')
 except Exception as e:
     print(f"Could not locate element: {e}")
@@ -40,12 +44,39 @@ print("Entered Analytics")
 search.click()
 sleep(2)
 try:
-    search = driver.find_element(By.XPATH, '''//table[@id="ctl00_ctl00_ContentPlaceHolder_BodyPlaceHolder_dlReports"]/tbody/tr[5]/td[2]/div[2]/span/span''')
+    search = driver.find_element(By.XPATH, '''//table[@id="ctl00_ctl00_ContentPlaceHolder_BodyPlaceHolder_dlReports"]/tbody/tr[5]/td[2]/div[2]/span''')
     print("Found run element")
 except Exception as e:
-    print(f"Could not locate element. Error: {e}")
+    print(f"Error loading page. Error: {e}")
     driver.close()
-print('finished search')
+sleep(1)
+search.click()
+try:
+    Xpath = '''//div[@id="divParams"]/div/table/tbody/tr[2]/td[1]/input'''
+    element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH, Xpath))
+    )
+    search = driver.find_element(By.XPATH, Xpath)
+except Exception as e:
+    print(f"Error loading page: {e}")
+
+now = datetime.datetime.now()
+todays_date = (now.strftime('%m/%d/%Y'))
+yesterday = now - timedelta(days = 1)
+yesterdays_date = yesterday.strftime('%m/%d/%Y')
+search.send_keys(str(yesterdays_date))
+search = driver.find_element(By.XPATH, '''//div[@id="divParams"]/div/table/tbody/tr[2]/td[2]/input''')
+search.send_keys(str(todays_date))
+search = driver.find_element(By.ID, "ctl00_ctl00_ContentPlaceHolder_BodyPlaceHolder_btnRunReport")
+search.click()
+sleep(3)
+search = driver.find_element(By.ID, "ctl00_ctl00_ContentPlaceHolder_BodyPlaceHolder_rptMSRSViewer_ctl05_ctl04_ctl00_ButtonLink")
+search.click()
+search = driver.find_element(By.XPATH, '''//div[@id="ctl00_ctl00_ContentPlaceHolder_BodyPlaceHolder_rptMSRSViewer_ctl05_ctl04_ctl00_Menu"]/div/a''')
+search.send_keys(Keys.RETURN)
+
+print('finished search.')
+
 
 
 
